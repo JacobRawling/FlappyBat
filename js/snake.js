0eb3c568b0fa151f4eb4f3d8e5e1047602744e83
jacobrawling <mrjacob3@gmail.com>
2016-04-21 00:11:28 +0100

    Added food and the ability to eat said food

--- js/snake.js
+++ js/snake.js
@@ -1,15 +1,17 @@
 //The Snake Object
-var Snake = function (length, cellWidth,ctx) {
+var Snake = function (length, cellWidth,ctx,canvasWidth,canvasHeight) {
   this.defaultLength  = length;
   this.length = length;
   this.cellWidth = cellWidth;
   this.ctx = ctx;
-  this.prevKey;
+  this.cWidth = canvasWidth;this.cHeight = canvasHeight;
   this.CreateSnake();
 }
 
 Snake.prototype.CreateSnake = function(){
   delete cells;
+
+  this.food = this.createFood();
   this.cells = [];
   this.direction = "right";
   var length  = 5;
@@ -20,18 +22,26 @@ Snake.prototype.CreateSnake = function(){
 }
 
 Snake.prototype.Paint = function(){
-  for(var i = 0; i < this.length; i++)
+  for(var i = 0; i < this.cells.length; i++)
   {
-    var cell = this.cells[i];
-    //Lets paint 10px wide cells
-    this.ctx.fillStyle = "blue";
-    this.ctx.fillRect(cell.x*this.cellWidth, cell.y*this.cellWidth, this.cellWidth, this.cellWidth);
-    this.ctx.strokeStyle = "white";
-    this.ctx.strokeRect(cell.x*this.cellWidth, cell.y*this.cellWidth, this.cellWidth, this.cellWidth);
+    this.PaintCell(this.cells[i].x,this.cells[i].y);
   }
+  this.PaintCell(food.x,food.y);
+}
+Snake.prototype.EatFood = function(){
+
+  if(this.direction == "right") this.food.x++;
+  else if(this.direction == "down") this.food.y++;
+  else if(this.direction == "left")this.food.x--;
+  else if(this.direction == "up") this.food.y--;
+  this.cells.unshift(this.food);
+  this.food = this.createFood();
 }
 
 Snake.prototype.ManageLogic = function(){
+  if(this.isCollides(food.x,food.y)){
+      this.EatFood();
+  }
   if(this.direction == "right") this.Move(1,0);
   else if (this.direction == "left") this.Move(-1,0);
   else if (this.direction == "up") this.Move(0,-1);
@@ -55,7 +65,7 @@ Snake.prototype.isCollides = function(x,y){
 }
 Snake.prototype.BoundaryCheck = function(x,y){
     head = this.cells[0];
-    if(x == -1 || y == -1 || x == 45 || y == 45)
+    if(x == -1 || y == -1 || x == Math.floor(this.cWidth/this.cellWidth )|| y == Math.floor(this.cHeight/this.cellWidth))
       return true;
     return false;
 }
@@ -63,7 +73,7 @@ Snake.prototype.BoundaryCheck = function(x,y){
 Snake.prototype.Move = function(x,y){
     newX = this.cells[0].x + x;
     newY = this.cells[0].y + y;
-    if(this.BoundaryCheck(newX,newY)){// || this.isCollides(newX,newY)){
+    if(this.BoundaryCheck(newX,newY) || this.isCollides(newX,newY)){
       this.CreateSnake();
       return;
     }
@@ -72,10 +82,24 @@ Snake.prototype.Move = function(x,y){
     tail.x = newX; tail.y = newY;
     this.cells.unshift(tail);
 }
+//Lets create the food now
+Snake.prototype.createFood = function()
+{
+	food = {
+		x: Math.round(Math.random()*(this.cWidth - this.cellWidth)/this.cellWidth),
+		y: Math.round(Math.random()*(this.cHeight - this.cellWidth)/this.cellWidth),
+	};
+  return food;
+}
+Snake.prototype.PaintCell = function(x,y){
+  this.ctx.fillStyle = "blue";
+	this.ctx.fillRect(x*this.cellWidth, y*this.cellWidth, this.cellWidth, this.cellWidth);
+	this.ctx.strokeStyle = "white";
+	this.ctx.strokeRect(x*this.cellWidth, y*this.cellWidth, this.cellWidth, this.cellWidth);
+}
 Snake.prototype.Update = function(){
   this.Paint();
   this.ManageLogic();
-
 }
 $(document).ready(function(){
 	var canvas = $("#canvas")[0];
@@ -83,7 +107,7 @@ $(document).ready(function(){
 	var w = $("#canvas").width();
 	var h = $("#canvas").height();
   var key;
-  var snake = new Snake(5,10,ctx);
+  var snake = new Snake(5,10,ctx,w,h);
 
   function PaintCanvas(){
   	ctx.fillStyle = "white";
